f63eb5e32275911ffe2941730ab106c88a3c47c2
Matthew Morgan <strongdogenergy@gmail.com>
2024-05-27 09:11:15 -0400

    with api calls from fronthend but no security

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # pool-navigator
-- Startup with `docker compose up`
+- Startup with `docker compose up` or `docker compose up --build`
 
-## Hugo platform 
-- https://dev.to/robinvanderknaap/setup-hugo-using-docker-43pm
+## Frontend web - Hugo platform 
+- Path `web`
+- https://dev.to/robinvanderknaap/setup-hugo-using-docker-43pm or https://gohugo.io/getting-started/quick-start/
+- Install hugo and verify `hugo version`
+- Create site `hugo new site web`
+- Add index.html to layouts folder
+- Configure docker compose
+```yaml
+web:
+    image: klakegg/hugo:0.107.0-ext-alpine
+    command: server -D --poll 700ms
+    volumes:
+    - "./web:/src"
+    ports:
+    - "8001:1313"
+```
+
+## Backend API - Python flask app
+- Path `api`
+- Python source, requirements.txt and Dockerfile
+- Configure docker compose
+```yaml
+api:
+    build: ./api
+    ports:
+        - "8000:5000"
+redis:
+    image: "redis:alpine"
+```
--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 import time
-
 import redis
 from flask import Flask
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -17,7 +19,31 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def get_secure_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('secure_hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 @app.route('/')
+@cross_origin()
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return {
+        "hits": count,
+    }
+
+@app.route('/secure')
+@cross_origin()
+def getSecureHits():
+    count = get_hit_count()
+    secure_count = get_secure_hit_count()
+    return {
+        "hits": count,
+        "secure_hits": secure_count,
+    }
